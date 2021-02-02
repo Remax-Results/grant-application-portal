80defa3f7b4ff259e7bc7b0a5927628fb1620ee8
Jordan Newberry <jordan.newberry@gmail.com>
2021-02-01 18:32:05 -0600

    grantAppForm work in progress. switching branches to dbFile branch

---
+++ src/components/GrantApplicationForm/GrantApplicationForm.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+
+
+function GrantApplicationForm(props) {
+  
+  // hooks
+  const [applicationData, setApplicationData] = useState({})
+
+
+  return (
+    <div>
+      <h2>{heading}</h2>
+    </div>
+  );
+}
+
+export default GrantApplicationForm;
