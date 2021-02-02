0b3fd69071e50474bc123746aa32376f381658fb
julianbooher <julianbooher@hotmail.com>
2021-02-02 15:16:58 -0600

    installed react-datepicker

--- src/components/GrantWindowSettings/GrantWindowSettings.jsx
+++ src/components/GrantWindowSettings/GrantWindowSettings.jsx
@@ -12,6 +12,7 @@ export default function GrantWindowSettings(props) {
     <div>
       {JSON.stringify(currentWindow)}
       <h2>Grant Window Settings</h2>
+      {currentWindow}
     </div>
   );
 }
