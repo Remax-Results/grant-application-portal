4b19bc866891b1643ff0ed84aa7dc273e28fd422
sdeda1us <steven.maloney@gmail.com>
2021-02-01 20:27:38 -0600

    reducer populates for admin table

--- server/routes/table.router.js
+++ server/routes/table.router.js
@@ -2,16 +2,21 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
+
 router.get('/', (req, res) => {
-  // GET route code here
+  sqlText = `SELECT  a.id, a.budget, a.date_received, 
+            u.org_name, u.contact_name, u.phone, u.username, 
+              f.focus, r.status   
+            FROM "user" as u
+            JOIN app AS a ON u.id=a.user_id
+            JOIN focus_area AS f ON a.focus_area_id=f.id
+            JOIN review_status AS r ON r.id=a.review_status_id;`;
+  pool.query(sqlText)
+  .then(result => {console.log(result.rows); res.send(result.rows)})
+  .catch(error=> console.log('Error retrieving app table data', error))
 });
 
-/**
- * POST route template
- */
+
 router.post('/', (req, res) => {
   // POST route code here
 });
