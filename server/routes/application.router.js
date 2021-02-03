64418ace26b594b46514b8f04aa6da03646e0c98
Jordan Newberry <jordan.newberry@gmail.com>
2021-02-02 21:30:44 -0600

    saving progress. application object is making it to the server on form submit

--- server/routes/application.router.js
+++ server/routes/application.router.js
@@ -13,7 +13,23 @@ router.get('/', (req, res) => {
  * POST route template
  */
 router.post('/', (req, res) => {
-  // POST route code here
+  // this function is taking the dynamic object coming over
+  // from the client, and for each piece of that object it will 
+  // make an insert into the database
+  console.log('if this goes off at least I know at least the thing made it here....');
+  const insertApp = (appData) => {
+    for (let question of appData) {
+      const sqlText = `INSERT INTO "app_question"("app_id", "question_id", "answer_text")
+                      VALUES ($1, $2, $3);`;
+      pool.query(sqlText, [question.id, question.id, question.answer_text])
+          .then(result => {
+            res.sendStatus(201);
+          }).catch(error => {
+            console.log('error adding application question answer to the database... ------>', error);
+            res.sendStatus(500);
+          });
+    }
+  };
 });
 
 module.exports = router;
