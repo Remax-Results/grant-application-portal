bb124bcad611ab531063e8d71e28e0f0d678f967
Steven Maloney <steven.maloney@gmail.com>
2021-02-06 18:12:59 -0600

    post is not working yet

--- src/components/AppDetails/Scoring/TenPointScale/TenPointScale.jsx
+++ src/components/AppDetails/Scoring/TenPointScale/TenPointScale.jsx
@@ -1,14 +1,18 @@
 import React, {useState} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 import MakeScale from './MakeScale.jsx';
 
 
 export default function TenPointScale() {
+    const dispatch=useDispatch();
     const numberLine = [...Array(11).keys()];
     const [selectedNumbers, setSelectedNumbers] = useState(0);
-
+    const qANDa = useSelector(state=>state.qANDa);
+    const detailsData = useSelector(state=>state.detailsData);
     return (
         <>
-        {numberLine.map((number) => (<MakeScale key={number} selected={selectedNumbers >= number} number={number} onSelect={()=>setSelectedNumbers(number)}/>))  }
+        {numberLine.map((number) => (<MakeScale key={number} selected={selectedNumbers >= number} number={number} onSelect={()=>{setSelectedNumbers(number); {dispatch({type:'UPDATE_SCORE', 
+                    payload: {score: selectedNumbers, q_id: qANDa.id, app_id:detailsData.id}})}}}/>))  }
         
         </>
     )
