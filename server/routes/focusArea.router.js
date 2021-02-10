88a63933978841dfd702554c5092f287aabb4ec9
Jordan Newberry <jordan.newberry@gmail.com>
2021-02-10 14:55:19 -0600

    added get route for grabbing active focus areas

--- server/routes/focusArea.router.js
+++ server/routes/focusArea.router.js
@@ -3,9 +3,19 @@ const pool = require('../modules/pool');
 const router = express.Router();
 const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
-// Get all of the options from the focus area table to populate the dropdown menu in the grant application.
+// Get all of focus areas for editing in question management
 router.get('/', rejectUnauthenticated, (req, res) => {
   const sqlText = `SELECT * FROM "focus_area";`;
+  pool.query(sqlText).then(result => {
+    res.send(result.rows);
+  }).catch((error) => {
+    console.log('Error retrieving list of focus areas from the DB... ----->', error);
+  });
+});
+
+// Get all active focus areas for the applications
+router.get('/active', rejectUnauthenticated, (req, res) => {
+  const sqlText = `SELECT * FROM "focus_area" WHERE "active"=true;`;
   pool.query(sqlText).then(result => {
     res.send(result.rows);
   }).catch((error) => {
--- src/redux/sagas/focusArea.saga.js
+++ src/redux/sagas/focusArea.saga.js
@@ -7,10 +7,25 @@ function* fetchFocusArea(action){
   yield put({ type: 'SET_FOCUS_AREA', payload: response.data });
 }
 
+function* changeFocusStatus(action) {
+  const { focusId, newStatus } = action.payload;
+  yield axios.put(`/api/question/question-status/${focusId}`, {newStatus: newStatus});
+  yield put({ type:'FETCH_FOCUS_AREA' });
+
+}
+
+function* changeFocusText(action) {
+  const { focusId, newText } = action.payload;
+  yield axios.put(`/api/question/question-text/${focusId}`, {newText: newText});
+  yield put({ type:'FETCH_FOCUS_AREA' });
+}
+
 
 //--------------------WATCHER SAGA---------------------------//
 function* focusAreaSaga() {
   yield takeLatest('FETCH_FOCUS_AREA', fetchFocusArea);
+  yield takeLatest('CHANGE_FOCUS_STATUS', changeFocusStatus);
+  yield takeLatest('CHANGE_FOCUS_TEXT', changeFocusText);
 }
 
 export default focusAreaSaga;
