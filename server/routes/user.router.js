794f38a48c32168e5776d63bb0ba7f299e248301
julianbooher <julianbooher@hotmail.com>
2021-02-10 13:48:43 -0600

    typo comment

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -15,7 +15,7 @@ const router = express.Router();
 const validateEmail = (email) => { 
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if(re.test(email)){
-      //Email valid. Procees to test if it's from the right domain (Second argument is to check that the string ENDS with this domain, and that it doesn't just contain it)
+      //Email valid. Proceed to test if it's from the right domain (Second argument is to check that the string ENDS with this domain, and that it doesn't just contain it)
       if(email.indexOf("@results.net", email.length - "@results.net".length) !== -1){
           //VALID
           return true
