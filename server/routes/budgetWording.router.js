306928273f1a6d3c7f5faf905e5d08c1aa75460c
julianbooher <julianbooher@hotmail.com>
2021-02-09 19:03:41 -0600

    put route initialized

--- server/routes/budgetWording.router.js
+++ server/routes/budgetWording.router.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
-const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+const { rejectUnauthenticatedAdmin } = require('../modules/admin-authentication-middleware');
 
 // Get wording for the budget question
 router.get('/', (req, res) => {
-    console.log('in router') 
     const sqlText = `SELECT * FROM budget_wording;`
     pool.query(sqlText)
     .then((result) => {
@@ -13,6 +12,20 @@ router.get('/', (req, res) => {
     }).catch((error) => {
         console.log('error retrieving budget wording from the database... -------->', error);
     });
-  });
+});
+
+router.put('/', rejectUnauthenticatedAdmin, (req, res, next) => {
+    const { updatedWording } = req.body;
+    const sqlText = `
+                    UPDATE budget_wording
+                    SET question_wording = $1
+                    ;`
+    pool.query(sqlText, [updatedWording])
+    .then(() => res.sendStatus(201))
+    .catch((err) => {
+        console.log('budgetWording PUT failed ', err);
+        res.sendStatus(500);
+    });
+});
 
   module.exports = router
