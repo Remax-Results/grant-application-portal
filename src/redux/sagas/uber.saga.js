2668b2974e69a41cf3f8439f9161b2f15394988f
Jordan Newberry <jordan.newberry@gmail.com>
2021-02-10 20:31:11 -0600

    added action type to the uber saga

--- src/redux/sagas/uber.saga.js
+++ src/redux/sagas/uber.saga.js
@@ -3,7 +3,7 @@ import { put, takeLatest } from 'redux-saga/effects';
 
 function* fetchFocusAreaQuestion(){
   yield put({ type: 'FETCH_ACTIVE_QUESTIONS' });
-  yield put({ type: 'FETCH_FOCUS_AREA' });
+  yield put({ type: 'FETCH_ACTIVE_FOCUS_AREA' });
   yield put({ type: 'FETCH_BUDGET_WORDING'});
 }
 
