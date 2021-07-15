c8d40da370ca0dbdfd53950bcf0bdba2ef8cb2f6
julianbooher <julianbooher@hotmail.com>
2021-07-15 18:43:06 -0500

    added error catch all for the reset saga

--- src/redux/sagas/reset.saga.js
+++ src/redux/sagas/reset.saga.js
@@ -8,32 +8,38 @@ function* sendReset(action){
     // Check to ensure inputted email exists.
     if (response.data.length > 0){
       // TODO - Potentially send the response instead of forwarding on the payload. Could include more data that way potentially.
-      yield put ({type: 'SEND_RESET_EMAIL', payload: action.payload})
+      yield put ({type: 'SEND_RESET_EMAIL', payload: action.payload});
     } else {
-      yield put ({type: 'EMAIL_DOES_NOT_EXIST'})
+      yield put ({type: 'EMAIL_DOES_NOT_EXIST'});
     }
   }
   catch(error){
     console.log('sendReset saga failed reset.saga.js', error);
+    yield put ({type: 'RESET_CATCH_ALL_SERVER'});
   }
 }
 
 function* sendResetEmail(action){
   try{
     yield axios.put(`/api/reset/email/${action.payload.username}`);
-    yield put ({type: 'CLEAR_RESET_ERROR'})
-    yield put ({type: 'EMAIL_SENT'})
+    yield put ({type: 'CLEAR_RESET_ERROR'});
+    yield put ({type: 'EMAIL_SENT'});
   }
   catch(error){
     console.log('sendResetEmail saga failed reset.saga.js', error);
+    yield put ({type: 'RESET_CATCH_ALL_SERVER'});
+
   }
 }
 
 function* fetchResetInfo(action){
   try{
+
     const {id, token} = action.payload
+    // check to ensure the reset token hasn't expired and the user id is correct.
     const check = yield axios.get(`/api/reset/check?id=${id}&token=${token}`);
-    // TODO conditional for result of the check.
+
+    // if the check comes back with data, it's correct and the form renders, if not, an expiration message is rendered.
     if (check.data.length > 0){
       yield put({type: 'CORRECT_RESET_PASSWORD_INFO'})
     }
